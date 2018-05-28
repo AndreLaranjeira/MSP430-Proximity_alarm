d9c8aa79d768a0bc446253dc06f0f28e4e05d214
André Laranjeira <AndreCaldasLaranjeira@gmail.com>
2018-05-28 18:26:07 -0300

    Initial structure for buttons.

--- Proximity alarm/main.c
+++ Proximity alarm/main.c
@@ -2,6 +2,7 @@
 #include "../Include/MSP430_shortcuts.h"
 
 // Global variables:
+unsigned int S1_q = 0, S2_q = 0;
 
 // Main function:
 void main(void) {
@@ -9,8 +10,64 @@ void main(void) {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
     InitializePorts();
+    ConfigS1();
+    ConfigS2();
+    ConfigLED1();
+    ConfigLED2();
 
-    LowPowerMode(3);
+    while(1) {
+
+        if((P2IN & BIT1) == 0) {    // Se S1 for pressionada.
+
+            if(S1_q == 1) {         // Se o estado anterior de S1 for "não pressionado".
+
+                // Colocar aqui o que acontece quando S1 é pressionado.
+                // Pressionando S1, deve-se ativar o alarme.
+
+                Debounce();         // Função de atraso para retirar debounces.
+                S1_q = 0;           // Mude o estado anterior de S1.
+
+            }
+
+        }
+
+        else {
+
+            if(S1_q == 0) {
+
+                S1_q = 1;       // Mude o estado anterior de S1.
+                Debounce();         // Função de atraso para retirar debounces.
+
+            }
+
+        }
+
+        if((P1IN & BIT1) == 0) {    // Se S2 for pressionada.
+
+            if(S2_q == 1) {         // Se o estado anterior de S2 for "não pressionado".
+
+                // Colocar aqui o que acontece quando S2 é pressionado.
+                // Pressionando S2, deve-se desativar o alarme.
+
+                Debounce();         // Função de atraso para retirar debounces.
+                S2_q = 0;           // Mude o estado anterior de S2.
+
+            }
+
+        }
+
+        else {
+
+            if(S2_q == 0) {
+
+                S2_q = 1;       // Mude o estado anterior de S2.
+                Debounce();         // Função de atraso para retirar debounces.
+
+            }
+
+        }
+
+    }
 
 }
 
