cc29898562281d1778e56759bb2d970968756b15
labm1997 <labm1997@gmail.com>
2018-06-22 12:08:52 -0300

    Estados implementados, falta testar e implementar comunicação com bluetooth e buzzer

--- Proximity alarm/main.c
+++ Proximity alarm/main.c
@@ -1,4 +1,5 @@
 #include <msp430.h>
+#include <math.h>
 #include <stdint.h>
 #include "../Include/MSP430_shortcuts.h"
 
@@ -15,21 +16,23 @@ typedef enum{
 
 // Function headers:
 void MeasureDistance(void);
+uint16_t RelativeError(uint16_t, uint16_t);
 
 // Global variables:
 uint8_t S1_q = 0, S2_q = 0, good_read = 0;
 uint16_t distance = 0, start = 0, end = 0;
+uint8_t MEASURE_BUSY = 0;
 
 // Main function:
 void main(void) {
 
 	// Local variable declarations:
 	State CurrentState = Idle;
-	unsigned int i;
+	uint16_t i, measures[3], average, relativeError;
 
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+		WDTCTL = WDTPW | WDTHOLD;	 // stop watchdog timer
 	
-    InitializePorts();	// Avoids energy loss to unitialized ports.
+		InitializePorts();	// Avoids energy loss to unitialized ports.
 	
 	/* Port usage:
 		P1.0: LED1,
@@ -42,30 +45,30 @@ void main(void) {
 	
 	// Switch configuration:
 	
-    ConfigS1();
-    ConfigS2();
+		ConfigS1();
+		ConfigS2();
 	
 	// LED configuration:
 	
-    ConfigLED1();
-    ConfigLED2();
+		ConfigLED1();
+		ConfigLED2();
 	
 	// Proximity sensor configuration:
 	
-	SetPort(P1, DIR, 2);
-    ClearPort(P1, OUT, 2);
+		SetPort(P1, DIR, 2);
+		ClearPort(P1, OUT, 2);
 
-    ClearPort(P2, DIR, 0);
-    SetPort(P2, SEL, 0);
+		ClearPort(P2, DIR, 0);
+		SetPort(P2, SEL, 0);
 
-    TA1CCTL1 = CaptureMode(3);
-    TA1CTL = TimerAConfiguration(SMCLK, 2);
+		TA1CCTL1 = CaptureMode(3);
+		TA1CTL = TimerAConfiguration(SMCLK, 2);
 	
-	__enable_interrupt();
+		__enable_interrupt();
 	
 	// Execution loop:
 
-    while(1) {
+		while(1) {
 
 		switch(CurrentState) {
 			
@@ -77,8 +80,11 @@ void main(void) {
 				SetPort(P1, OUT, 0);	// Liga o LED1.
 				ClearPort(P4, OUT, 7);	// Desliga o LED2.
 			
-				while(CurrentState == Idle) {
-					
+				if(ComparePortEQ(P2, IN, 1, 0)){
+					Debounce();
+					CurrentState = Arming;
+					while(ComparePortEQ(P2, IN, 1, 0));
+					Debounce();
 				}
 			
 				break;
@@ -90,14 +96,31 @@ void main(void) {
 				// entrar no estado Set. Caso contrário, deve-se entrar no
 				// estado Idle.
 			
-				while(CurrentState == Arming) {
-					
-					for(i = 3; i > 0; --i) {
-					
-					}
-					
+				for(i=0,average=0;i<3;i++) {
+					MeasureDistance();
+					while(MEASURE_BUSY);
+					measures[i] = distance;
+					average += distance;
 				}
-			
+				
+				average /= 3;
+				
+				// Verifica se algum dos erros percentuais com relação à média
+				// é maior que 5%
+				
+				for(i=0;i<3;i++) {
+					// Essa conta é muito complicada?
+					if(RelativeError(average, measures[i]) > 5){
+						CurrentState = Idle;
+						break;
+					}
+				}
+				
+				if(CurrentState == Idle) break;
+				
+				CurrentState = Set;
+				
+				
 				break;
 				
 			case Set:
@@ -109,10 +132,20 @@ void main(void) {
 			
 				ClearPort(P1, OUT, 0);	// Desliga o LED1.
 				SetPort(P4, OUT, 7);	// Liga o LED2.
-			
-				while(CurrentState == Set) {
-					
+				
+				if(ComparePortEQ(P1, IN, 1, 0)){
+					Debounce();
+					CurrentState = Idle;
+					while(ComparePortEQ(P1, IN, 1, 0));
+					Debounce();
+					break;
 				}
+			
+				MeasureDistance();
+				while(MEASURE_BUSY);
+				
+				if(RelativeError(average, distance) > 5) 
+					CurrentState = Triggered;
 			
 				break;
 				
@@ -123,54 +156,84 @@ void main(void) {
 				// acender o LED1 e o LED2 alternadamente. Quando o botão S2
 				// for apertado, deve-se ir para o estado Idle.
 				
-				while(CurrentState == Triggered) {
-					
-				}
+				
+				TA0CTL = TACLR | TASSEL__ACLK | MC_0;
+				TA0CCR0 = 32768; // 1s
+				TA0CCTL0 |= CCIE;				
+				TA0CTL |= MC_2;
+				
+				// Enviar notificação e ligar o buzzer
+				
+				// Volta ao Idle
+				
+				while(ComparePortEQ(P1, IN, 1, 0)){
+					Debounce();
+					CurrentState = Idle;
+					while(ComparePortEQ(P1, IN, 1, 0));
+					Debounce();
+				}				
 				
 				break;
 			
 		}
 
-    }
+		}
 
 }
 
 // Function implementations:
 void MeasureDistance(void) {
 	SetPort(P1, OUT, 2);
-    ClearPort(P1, OUT, 2);
+	// We could put a delay, but not necessary as we tested
+	ClearPort(P1, OUT, 2);
+	MEASURE_BUSY = 1;
+}
+
+uint16_t RelativeError(uint16_t average, uint16_t distance){
+	return 100*abs(average-distance)/average;
 }
 
 // ISRs:
 #pragma vector = TA1_CCRN_INT
 __interrupt void TA1_CCRN_ISR() {
 
-    switch(TA1IV) {
+		switch(TA1IV) {
 
 		// Proximity sensor echo interruption:
 	
-        case 0x02:
+				case 0x02:
 
 			// Measurement started.
 		
-            if((TA1CCTL1 & CCI) == CCI) {
-                start = TA1CCR1;
-                good_read = 1;
+						if((TA1CCTL1 & CCI) == CCI) {
+								start = TA1CCR1;
+								good_read = 1;
 
-            }
+						}
 			
 			// Measurement ended.
 
-            if((TA1CCTL1 & CCI) == 0) {
-                if(good_read == 1) {
-                    end = TA1CCR1;
-                    distance = (end - start)/(PROX_SENSOR_CONSTANT);
-					good_read = 0;
-                }
-            }
+						if((TA1CCTL1 & CCI) == 0) {
+								if(good_read == 1) {
+										end = TA1CCR1;
+										distance = (end - start)/(PROX_SENSOR_CONSTANT);
+										good_read = 0;
+										MEASURE_BUSY = 0;
+								}
+						}
 
-        break;
+				break;
+				
+				
 
-    }
+		}
 
+}
+
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TA0_CCR0_ISR(){
+      TogglePort(P1, OUT, 0);
+      TogglePort(P4, OUT, 7);
+      TA0CCTL0 &= ~CCIFG;
 }
